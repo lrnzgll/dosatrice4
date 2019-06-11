6027402c5ea1d80033d38a4006d6449bcc5a1770
lorenzo galli <lrnzgll@gmail.com>
2019-06-11 22:25:18 +0100

    qwe

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,2 @@
 import "bootstrap";
-//import "../stylesheets/applications.scss";
 require.context('../stylesheets/images', true, /\.(?:png|jpg|gif|ico|svg)$/)
-var Turbolinks = require("turbolinks")
-Turbolinks.start()
-
