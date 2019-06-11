15977ee1725cecc954a8862bb39dfd4ca6366762
lorenzo galli <lrnzgll@gmail.com>
2019-06-11 22:10:12 +0100

    ggg

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import "bootstrap";
-import "../stylesheets/applications.scss";
+//import "../stylesheets/applications.scss";
 require.context('../stylesheets/images', true, /\.(?:png|jpg|gif|ico|svg)$/)
 var Turbolinks = require("turbolinks")
 Turbolinks.start()
