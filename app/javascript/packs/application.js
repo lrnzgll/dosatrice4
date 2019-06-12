76d5567971ed5ff4d8d9f76486492d3e7b2af30b
lorenzo galli <lrnzgll@gmail.com>
2019-06-12 20:49:39 +0100

    fix language locale

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,4 @@
 import "bootstrap";
 require.context('../stylesheets/images', true, /\.(?:png|jpg|gif|ico|svg)$/)
+import Turbolinks from "turbolinks"
+Turbolinks.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get '/prodotti', to: 'pages#products'
-  get '/dovesiamo', to: 'pages#where'
-  get '/language', to: 'languages#change'
+  scope "(:locale)", locale: /en|it/ do
+    root to: 'pages#home'
+    get '/prodotti', to: 'pages#products'
+    get '/dovesiamo', to: 'pages#where'
+  end
 end
