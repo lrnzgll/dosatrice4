c094265c0a7622e9b20f21c548d8dd4785936e9a
lorenzo galli <lrnzgll@gmail.com>
2019-06-15 14:24:42 +0100

    add js map

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.serve_static_assets = false
+  config.assets.digest = true
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -15,7 +17,7 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
+    config.action_controller.perform_caching = false
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
@@ -23,8 +25,7 @@ Rails.application.configure do
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
-    config.action_controller.perform_caching = true
-
+    config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
 
